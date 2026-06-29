0a4a807b3b4ae29431dff88afb322a8be734a10d
Tsunhua Yang <297312757+tshyang-nycu@users.noreply.github.com>
2026-06-29 09:35:50 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "softball-scorebook-v16";
+const CACHE_NAME = "softball-scorebook-v17";
 const ASSETS = [
   "./",
   "./index.html",
-  "./style.css?v=16",
-  "./app.js?v=16",
+  "./style.css?v=17",
+  "./app.js?v=17",
   "./manifest.webmanifest",
   "./logo.png",
 ];
